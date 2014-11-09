4c79a7e6faca95de6ce0ef60141f07646fcbec39
Peter Elger <elger.peter@gmail.com>
2014-11-09 23:47:06 +0000

    added prod

--- srvs/info-prod.js
+++ srvs/info-prod.js
@@ -1,10 +1,9 @@
-
 var seneca = require('seneca')()
-      .use('redis-transport')
-      .use('../info.js')
+  .use('redis-transport')
+  .use('../info.js')
 
-      .client({type:'redis',pin:'role:info,req:part'})
-      .listen({type:'redis',pin:'role:info,res:part'})
+  .client({host: '192.168.59.103', type:'redis',pin:'role:info,req:part'})
+  .listen({host: '192.168.59.103', type:'redis',pin:'role:info,res:part'})
 
-      .listen()
+  .listen();
 
