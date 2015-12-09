1abffa4d70b06fe0d41d612d19b7118da9469635
Richard Rodger <richard@ricebridge.com>
2015-12-09 21:03:11 +0000

    mesh

--- srv/info-dev.js
+++ srv/info-dev.js
@@ -1,14 +1,9 @@
-var REDIS = process.env.REDIS || 'localhost'
 var STATS = process.env.STATS || 'localhost'
 
 require('seneca')()
   .use('../info.js')
-  .repl(43001)
-  .listen(44001)
-
-  .use('redis-transport')
-  .client({ host:REDIS, type:'redis', pin:'role:info,req:part' })
-  .listen({ host:REDIS, type:'redis', pin:'role:info,res:part' })
+  .repl(33001)
+  .use('mesh',{auto:true, pin:['role:info,cmd:get','role:info,res:*']})
 
   .use('msgstats',{
     udp: { host: STATS },
