9f04a4147161d20ec69688ed991aaf16cf390271
Richard Rodger <richard@ricebridge.com>
2015-12-17 12:01:25 +0000

    no repl

--- srv/info-dev.js
+++ srv/info-dev.js
@@ -2,7 +2,6 @@ var STATS = process.env.STATS || 'localhost'
 
 require('seneca')()
   .use('../info.js')
-  .repl(33001)
   .use('mesh',{auto:true, pin:['role:info,cmd:get','role:info,res:*']})
 
   .use('msgstats',{
