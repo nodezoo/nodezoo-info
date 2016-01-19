cc8092b81c38787660563753dc1958038b412969
Matthew O'Connor <matthew.oconnor@nearform.com>
2016-01-19 17:25:21 +0000

    first-pass

--- info.js
+++ info.js
@@ -1,50 +1,52 @@
 /* Copyright (c) 2014-2015 Richard Rodger, MIT License */
 /* jshint node:true, asi:true, eqnull:true */
-"use strict";
+'use strict';
 
 
-var lru = require('lru-cache')
+const LRU = require('lru-cache');
 
 
 module.exports = function info( options ){
-  var seneca = this
 
-  options = seneca.util.deepextend({
-    size: 99999,
-    wait: 222
-  },options)
+    const seneca = this;
 
-
-  var info_cache = lru( options.size )
+    options = seneca.util.deepextend({
+        size: 99999,
+        wait: 222
+    },options);
 
 
-  seneca.add( 'role:info,cmd:get', cmd_get )
-  seneca.add( 'role:info,res:part', res_module )
+    const info_cache = LRU( options.size );
 
 
-  function cmd_get( args, done ) {
-    var seneca  = this
-
-    var name = args.name
-    seneca.act('role:info,req:part',{name:name})
-
-    setTimeout(function(){
-      
-      var data = info_cache.get( name ) || {}
-      done(null,data)
-
-    },options.wait)
-  }
+    seneca.add( 'role:info,cmd:get', cmd_get );
+    seneca.add( 'role:info,res:part', res_module );
 
 
-  function res_module( args, done ) {
-    var name = args.name
-    
-    var data = info_cache.get( name ) || {}
-    data[ args.part ] = args.data
-    info_cache.set( name, data )
+    const cmd_get = function ( args, done ) {
 
-    done()
-  }
-  
-}
+        const senec  = this;
+        const name = args.name;
+        senec.act('role:info,req:part',{ name:name });
+
+        setTimeout(function (){
+
+            const data = info_cache.get(name) || {};
+            done(null,data);
+
+        },options.wait);
+    };
+
+
+    res_module = function (args, done) {
+
+        const name = args.name;
+
+        const data = info_cache.get( name ) || {};
+        data[ args.part ] = args.data;
+        info_cache.set( name, data );
+
+        done();
+    };
+
+};
--- srv/info-dev.js
+++ srv/info-dev.js
@@ -1,10 +1,12 @@
-var STATS = process.env.STATS || 'localhost'
+'use strict';
+
+const STATS = process.env.STATS || 'localhost';
 
 require('seneca')()
   .use('../info.js')
-  .use('mesh',{auto:true, pin:['role:info,cmd:get','role:info,res:*']})
+  .use('mesh',{ auto:true, pin:['role:info,cmd:get','role:info,res:*'] })
 
-  .use('msgstats',{
-    udp: { host: STATS },
-    pins:['role:info,req:part','role:info,res:part']
-  })
+    .use('msgstats',{
+        udp: { host: STATS },
+        pins:['role:info,req:part','role:info,res:part']
+    });
