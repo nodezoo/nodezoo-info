15bf3bc40760d39bdc59525af1d328f93d95002d
Dean McDonnell <mcdonnelldean@outlook.com>
2016-02-20 13:54:40 +0000

    Adding in new metrics bits

--- srv/info-dev.js
+++ srv/info-dev.js
@@ -1,13 +1,26 @@
 var STATS = process.env.STATS || 'localhost'
 
 var opts = {
+  metrics: {
+    emitter: {enabled: true}
+  },
+  seneca_metrics: {
+    pins: [
+      'role:info,req:part',
+      'role:info,res:part'
+    ]
+  },
   mesh: {
     auto: true,
-    pin: ['role:info,cmd:get', 'role:info,res:*']
+    pin: [
+      'role:info,cmd:get',
+      'role:info,res:*'
+    ]
   }
 }
 
 require('seneca')()
   .use('../info.js')
+  .use('vidi-metrics', opts.metrics)
+  .use('vidi-seneca-metrics', opts.seneca_metrics)
   .use('mesh', opts.mesh)
-  .listen(44001)
