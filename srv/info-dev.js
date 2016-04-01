771352ad2c6b9e19d161661715f90a3b1404574d
Matthew O'Connor <matthewocsecond@gmail.com>
2016-04-01 15:31:45 +0100

    moved entity plugin

--- srv/info-dev.js
+++ srv/info-dev.js
@@ -26,8 +26,9 @@ var opts = {
 }
 
 require('seneca')()
+  .use('entity')
   .use('../info.js')
   .use('vidi-metrics', opts.metrics)
   .use('vidi-seneca-metrics', opts.seneca_metrics)
   .use('mesh', opts.mesh)
-  .use('entity')
+  
