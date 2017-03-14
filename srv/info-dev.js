1fa0f8610fd34b03e3fcb845ee1209a64dd5dc5e
Richard Rodger <richard@ricebridge.com>
2017-03-14 13:57:52 +0000

    info-dev

--- srv/info-dev.js
+++ srv/info-dev.js
@@ -1,10 +1,2 @@
-var STATS = process.env.STATS || 'localhost'
-
-require('seneca')()
+require('seneca')({tag: 'info'})
   .use('../info.js')
-  .use('mesh',{auto:true, pin:['role:info,cmd:get','role:info,res:*']})
-
-  .use('msgstats',{
-    udp: { host: STATS },
-    pins:['role:info,req:part','role:info,res:part']
-  })
