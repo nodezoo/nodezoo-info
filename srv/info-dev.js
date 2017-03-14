8a26f5b66e10fe405b03cc6a7f5a52d3134fba42
Richard Rodger <richard@ricebridge.com>
2017-03-14 14:08:38 +0000

    mock

--- srv/info-dev.js
+++ srv/info-dev.js
@@ -1,3 +1,8 @@
+/* Copyright (c) 2014-2017 Richard Rodger and other contributors, MIT License */
+
+var MOCK_NEED = JSON.parse(process.env.MOCK_NEED || 'false')
+
+
 var Seneca = require('seneca')
 
 Seneca({tag: 'info'})
