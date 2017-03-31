074d6f82832c97dbe48f052a0158826b20d605cd
Richard Rodger <richard@ricebridge.com>
2017-03-31 14:28:08 +0100

    stage

--- srv/info-stage.js
+++ srv/info-stage.js
@@ -7,15 +7,8 @@ var Seneca = require('seneca')
 Seneca({tag: 'info'})
   .listen(PORT)
 
+  .use('redis-transport')
   .use('../info.js')
 
-  .add('role:info,need:part', function (msg, reply) {
-    reply()
-
-    this
-      .act(msg,{part:'npm'})
-      .act(msg,{part:'github'})
-  })
-
-  .client({pin:'role:info,need:part,part:npm', host:'npm', port:PORT})
-  .client({pin:'role:info,need:part,part:github', host:'github', port:PORT})
+  .client({pin:'role:info,need:part', type:'redis', host:'redis'})
+  .listen({pin:'role:info,collect:part', type:'redis', host:'redis'})
