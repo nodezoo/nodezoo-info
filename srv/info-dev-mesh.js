4bf709e96a89f4e87168190800fc831e2e71a30a
Richard Rodger <richard@ricebridge.com>
2017-05-24 16:58:14 +0000

    msdlon

---
+++ srv/info-dev-mesh.js
@@ -0,0 +1,19 @@
+/* Copyright (c) 2014-2017 Richard Rodger and other contributors, MIT License */
+
+
+var Seneca = require('seneca')
+
+Seneca({tag: 'info'})
+  .test()
+
+  .use('monitor')
+
+  .use('mesh', {
+    listen: [
+      {pin:'role:info,cmd:get', model:'consume'},
+      {pin:'role:info,collect:part', model:'observe'},
+    ]
+  })
+
+  .use('../info.js')
+
