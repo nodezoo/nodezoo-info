d547eecbd405d3decdc56994aca11d3bfe7ed0e7
Richard Rodger <richard@ricebridge.com>
2017-11-02 18:33:18 +0000

    consul

--- srv/info-prod.js
+++ srv/info-prod.js
@@ -1,11 +1,16 @@
 /* Copyright (c) 2014-2017 Richard Rodger and other contributors, MIT License */
 
-var BASES = process.env.BASES.split(',')
+//var BASES = process.env.BASES.split(',')
+var CONSUL = process.env.CONSUL_SERVICE_HOST || 'localhost'
 
 var Seneca = require('seneca')
 
 Seneca({tag: 'info'})
   .test('print')
+
+  .use('consul-registry', {
+    host: CONSUL
+  })
 
   .use('../info.js')
 
@@ -14,7 +19,12 @@ Seneca({tag: 'info'})
       {pin: 'role:info,cmd:get'},
       {pin: 'role:info,collect:part', model:'observe'}
     ],
-    bases: BASES,
+    //bases: BASES,
     host: '@eth0',
-    sneeze: {silent:false}
+    //sneeze: {silent:false}
+    discover: {
+      registry: {
+        active: true
+      }
+    }
   })
